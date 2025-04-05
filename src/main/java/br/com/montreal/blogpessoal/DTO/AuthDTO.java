c72361c171f79d1f5ec5341f7a027188af32235c
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-04-05 09:17:36 -0300

    :sparkles: feat: DTOs

---
+++ src/main/java/br/com/montreal/blogpessoal/DTO/AuthDTO.java
@@ -0,0 +1,12 @@
+package br.com.montreal.blogpessoal.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthDTO(
+        @Email(message = "O email deve ser válido")
+        @NotBlank(message = "O email não pode estar vazio")
+        String email,
+
+        @NotBlank(message = "A senha não pode estar vazia")
+        String senha) {}
