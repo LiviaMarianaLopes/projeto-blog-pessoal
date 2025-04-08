71ca03181712bf4240fd1c3434fa8a2f33316b26
Lívia Mariana Lopes <liviamariana2004@gmail.com>
2025-04-08 17:03:29 -0300

    :sparkles: feat: queries adicionais

--- src/main/java/br/com/montreal/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/br/com/montreal/blogpessoal/repository/PostagemRepository.java
@@ -2,6 +2,11 @@ package br.com.montreal.blogpessoal.repository;
 
 import br.com.montreal.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+    @Query("SELECT p FROM Postagem p WHERE (:autorId IS NULL OR p.usuario.id = :autorId) AND (:temaId IS NULL OR p.tema.id = :temaId)")
+    List<Postagem> filtrarPorAutorETema(@Param("autorId") Long autorId, @Param("temaId") Long temaId);
 }
